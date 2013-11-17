e245874ded82ab2b3e15953fb5ee691d767234e6
Marco Vassena <m.vassena@students.uu.nl>
2013-11-17 10:36:14 +0100

    Added Bibtex module and datatypes

---
+++ project1/src/Bibtex.hs
@@ -0,0 +1,25 @@
+-- | This module defines the datatypes used to model a Bibtex database.
+
+module Bibtex where
+
+-- | A Bibtex database is a list of Bibtex Entry
+type BibtexDb = [BibtexEntry]
+
+-- | A Bibtex entry has a specific type and contains some data
+data BibtexEntry = Entry Type Data
+
+-- | Represents the type of a Bibtex entry
+type Type = String
+
+-- | An entry data is composed by a unique identifier ('Key') and
+-- the related fields
+data Data = Data Key [(Field, Value)]
+
+-- | Represents a unique identifier
+type Key = String
+
+-- | Represents the name of a field
+type Field = String
+
+-- | Represents the content of a field
+type Value = String
