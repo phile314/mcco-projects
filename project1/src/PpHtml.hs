8c61744a702446b78e85f78c5c7846957275717a
Philipp Hausmann <philipp.hausmann@314.ch>
2013-11-19 12:18:40 +0100

    -Escaping should work now in our code, but the library seems to be buggy

--- project1/src/PpHtml.hs
+++ project1/src/PpHtml.hs
@@ -27,7 +27,7 @@ flattenHtmlTree = component (\n -> do
 --TODO escaping
 
 -- | Converts a `Node` to a `Doc`.
-toDoc (Text s) = text s
+toDoc (Text s) = escapedAll s
 toDoc (Elem t [] attrs) = do
     enclose langle (text "/" >|< rangle) (text t >|< toDocA attrs)
 toDoc (Elem t es attrs) = do
@@ -36,14 +36,33 @@ toDoc (Elem t es attrs) = do
         >-< (enclose (langle >|< text "/") rangle (text t))
 
 
---TODO escaping
-
+-- I'm not really sure what are valid characters in xml names, hence we do not escape them for the time being...
 toDocA :: [(AttrName, AttrVal)] -> Doc
 toDocA [] = empty
-toDocA ((an, av):as) = space >|< text an >|< text "=" >|< (quoted $ text av) >|< toDocA as
+toDocA ((an, av):as) = space >|< text an >|< text "=" >|< (quoted $ escaped (=='\"') av) >|< toDocA as
 
 quoted d = enclose q q d
     where q = text "\""
 
 toDocE :: [Node] -> Doc
 toDocE es = foldl (\a c -> a >|< (toDoc c)) empty es
+
+escaped :: (Char -> Bool) -> String -> Doc
+escaped p str = text (escape p str)
+
+-- | Escape all characters for which xml entities are definied.
+escapedAll = escaped (const True)
+
+-- | Escape characters if the predicate returns true. If the predicate is false or
+--   there is no escaping mechanism defined for a character, the original character
+--   is retained.
+escape :: (Char -> Bool) -> String -> String
+escape _ []     = ""
+escape p (s:ss) = (if (p s) then f s else [s]) ++ (escape p ss)
+    where        
+        f '<' = "&lt;"
+        f '>' = "&gt;"
+        f '&' = "&amp;"
+        f '\'' = "&apos;"
+        f '\"' = "&quot;"
+        f s   = [s]
