77e004ee3bffa35618f220af96e2d116aeacdb7a
Marco Vassena <m.vassena@students.uu.nl>
2013-11-20 12:50:10 +0100

    Forgot to commit this

--- project1/src/Bibtex.hs
+++ project1/src/Bibtex.hs
@@ -2,6 +2,10 @@
 
 module Bibtex where
 
+import CCO.Tree (ATerm (App), Tree (fromTree, toTree))
+import CCO.Tree.Parser (parseTree, app, arg)
+import Control.Applicative
+
 -- | A Bibtex database is a list of Bibtex Entry
 type BibtexDb = [BibtexEntry]
 
@@ -21,3 +25,10 @@ type Field = String
 
 -- | Represents the content of a field
 type Value = String
+
+-------------------------------------------------------------------------------
+-- ATerm
+-------------------------------------------------------------------------------
+instance Tree BibtexEntry where
+  fromTree (Entry t k fv) = App "Entry" [fromTree t, fromTree k, fromTree fv]
+  toTree = parseTree [app "Entry" (Entry <$> arg <*> arg <*> arg)]
