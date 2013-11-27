557fd0cd3bd14d7e5dcec262dd84afe3d57930e2
Philipp Hausmann <philipp.hausmann@314.ch>
2013-11-27 12:50:54 +0100

    -Fixed test cases

--- project1/testsuite/FieldTransformerTest.hs
+++ project1/testsuite/FieldTransformerTest.hs
@@ -7,56 +7,72 @@ module Main where
 import Bibtex
 import BibHtml.Spec
 import BibHtml.Converter
-import CCO.Feedback (runFeedback)
 import Data.Maybe (isJust)
 import Data.List (lookup) 
-import Html.Tree (HtmlTree)
+import CCO.Feedback
+import Html.Tree
 
 import System.Exit (exitFailure)
-import System.IO (stderr)
 import Test.QuickCheck
 import Test.QuickCheck.Test (isSuccess)
-import Test.QuickCheck.Monadic
+import Test.QuickCheck.Assertions
+import Test.QuickCheck.Property (succeeded)
 import Utility
 
--- | Returns the expected result when running the AG using the given
--- specification tree and bibtex entry.
-expected :: BibtexEntry -> SpecTree -> [HtmlTree]
-expected e s = html_Syn_SpecTree $ walkTree fieldToHtml e s
+
+html :: BibtexEntry -> SpecTree -> [HtmlTree]
+html e s = html_Syn_SpecTree $ walkTree fieldToHtml e s
+
+msgs :: BibtexEntry -> SpecTree -> [Message]
+msgs e s = msgs_Syn_SpecTree $ walkTree fieldToHtml e s
+
+
+-- | Asserts that either both spec are not available or both return the same tree for a given bitex entry.
+assertEqualAvailHtml :: BibtexEntry -> SpecTree -> SpecTree -> Property
+assertEqualAvailHtml entry act exp =
+    avail_Syn_SpecTree rAct ?== avail_Syn_SpecTree rExp
+    .&.
+    (avail_Syn_SpecTree rExp ==> html_Syn_SpecTree rAct ?== html_Syn_SpecTree rExp)
+    where rAct = walkTree fieldToHtml entry act
+          rExp = walkTree fieldToHtml entry exp
+
 
 prop_either :: BibtexEntry -> SpecTree -> SpecTree -> Property
-prop_either e s1 s2 = lbl [s1, s2] $ monadicIO $ wp m test
-  where m = runFeedback (entryToHtml e) 1 1 stderr
-        t = Either s1 s2    -- FIX Should be generated directly with a generator
-        test Nothing = error "Validation failed"
-        test (Just actual) = assert $ 
-          (isAvail e t) == (isAvail e s1 || isAvail e s2)
-          &&
-          case (isAvail e s1, isAvail e s2) of
-            (True, _)      ->  actual == expected e s1
-            (False, True)  ->  actual == expected e s2
-            (False, False) -> error "Computation should already be aborted" 
+prop_either e s1 s2 = lbl [s1, s2] $
+    (isAvail e t) ?== (isAvail e s1 || isAvail e s2)
+    .&.
+    (isAvail e t ==>
+        case (isAvail e s1, isAvail e s2) of
+            (True, _)      -> assertEqualAvailHtml e t s1
+            (False, True)  -> assertEqualAvailHtml e t s2
+    )
+    where t = Either s1 s2
 
 prop_both :: BibtexEntry -> SpecTree -> SpecTree -> Property
-prop_both e s1 s2 = lbl [s1, s2] $ monadicIO $ wp m test
-  where m = runFeedback (entryToHtml e) 1 1 stderr
-        t = (Both s1 s2)  -- FIX Should be generated directly with a generator
-        test Nothing = error "Validation failed"
-        test (Just actual) = assert $ ((isAvail e t) == (isAvail e s1 && isAvail e s2))
-                      && (actual == (expected e s1) ++ (expected e s2)) -- Not sure about this
+prop_both e s1 s2 = lbl [s1, s2] $
+    (isAvail e t) ?== (isAvail e s1 && isAvail e s2)
+    .&.
+    (isAvail e t ==> (html e t) ?== ((html e s1) ++ (html e s2)))
+    where t = (Both s1 s2)
 
 prop_optional :: BibtexEntry -> SpecTree -> Property
-prop_optional e s = lbl [s] $ monadicIO $ wp m test
-  where m = runFeedback (entryToHtml e) 1 1 stderr
-        test Nothing = error "Validation failed"
-        test (Just actual) = assert $ (isAvail e (Optional s) == True) && (
-          case isAvail e (Optional s) of
-            True  -> actual == expected e (Optional s)
-            False -> actual == expected e s)
+prop_optional e s = lbl [s] $
+    isAvail e (Optional s) ?== True
+    .&.
+    case isAvail e s of
+        True  -> assertEqualAvailHtml e t s
+        False -> (binAsrt "Unavailable optional elements must not generate messages." (null $ msgs e t)) .&. (html e t ?== [])
+    where t = Optional s
 
 prop_exactly :: BibtexEntry -> Field -> Property
 prop_exactly e@(Entry _ _ fs) f = property $
-    isAvail e (Exactly f) == (isJust $ lookup f fs)
+    isAvail e (Exactly f) ?== (isJust $ lookup f fs)
+
+prop_avail_msgs :: BibtexEntry -> SpecTree -> Property
+prop_avail_msgs e s = lbl [s] $
+    isAvail e s ?/= any isError (msgs e s)
+    where isError (Error _) = True
+          isError _         = False
 
 
 -- | The entry point of the test suite
@@ -66,7 +82,8 @@ main = do
   r2 <- quickCheckResult prop_both
   r3 <- quickCheckResult prop_optional
   r4 <- quickCheckResult prop_exactly
-  if passQ [r1, r2, r3, r4]
+  r5 <- quickCheckResult prop_avail_msgs
+  if passQ [r1, r2, r3, r4, r5]
     then return ()
     else exitFailure
 
