56ca28e2a55f32e94e11ff7533dbafa2cda04f03
Philipp Hausmann <philipp.hausmann@314.ch>
2013-11-27 15:02:48 +0100

    -Moved utility parsers to parser file

--- project1/src/lib/Parser.hs
+++ project1/src/lib/Parser.hs
@@ -58,3 +58,33 @@ pAt = pToken AtSign
 
 pEq :: TokenParser Token
 pEq = pToken EqualSign
+-- This module defines a lexer for a Bibtex database
+
+
+
+-------------------------------------------------------------------------------
+-- Utility Parser
+-------------------------------------------------------------------------------
+
+-- | Parses a given token and returns it
+pToken :: Token -> Parser Token Token
+pToken t = satisfy (t ==) <!> describe t ""
+
+-- | Parses a value token and returns the string content
+pValue :: Parser Token String
+pValue = (\(Value s) -> s) <$> satisfy isValue 
+
+-- | Parses an identifier token and returns the string content
+pIdentifier :: Parser Token String
+pIdentifier = (\(Identifier s) -> s) <$> satisfy isIdentifier  
+                                     <!> describe (Identifier "") ""
+
+-- | Tests whether a token is an identifier
+isIdentifier :: Token -> Bool
+isIdentifier (Identifier _) = True
+isIdentifier  _             = False
+
+-- | Tests whether a token is a value
+isValue :: Token -> Bool
+isValue (Value _) = True
+isValue  _        = False
