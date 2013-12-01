d3dd154e60725fd7d6f4d23b83d12291a60ce515
Marco Vassena <m.vassena@students.uu.nl>
2013-12-01 18:32:57 +0100

    Refactoring
      Corrected year position for Book and Inproceeding

--- project1/src/lib/BibHtml/BibtexSpec.hs
+++ project1/src/lib/BibHtml/BibtexSpec.hs
@@ -27,17 +27,31 @@ sVN = optional (Volume <|> Number)
 --   REMARK: Please note that the order is incorrect for many entry types, as this information
 --   is hard to get by. To rectify the wrong ordering, the fields can just be moved to the proper location below.
 spec :: Type -> SpecTree
-spec Article = allOf [sAE, e Title, e Journal, e Year, o Volume, o Number, o Pages, o Month, o Note]
-spec Book = allOf [sAE, e Title, e Publisher, e Year, sVN, o Series, o Address, o Edition, o Month, o Note]
-spec Booklet = allOf [e Title, o Author, o HowPublished, o Address, o Month, o Year, o Note]
-spec Conference = spec Inproceedings
-spec Inbook = allOf [sAE, e Title, someOf (e Chapter) (e Pages), e Publisher, e Year, o Volume, sVN, o Series, o Type, o Address, o Edition, o Month, o Note]
-spec Incollection = allOf [e Author, e Title, e Booktitle, e Year, o Editor, sVN, o Series, o Pages, o Address, o Month, o Organization, o Publisher, o Note]
-spec Inproceedings = allOf [e Author, e Title, o Editor, e Booktitle, e Year, sVN, o Series, o Pages, o Address, o Month, o Organization, o Publisher, o Note]
-spec Manual = allOf [e Title, o Author, o Organization, o Address, o Edition, o Month, o Year, o Note]
-spec Mastersthesis = allOf [e Author, e Title, e School, e Year, o Type, o Address, o Month, o Note]
-spec Misc = allOf [o Author, o Title, o HowPublished, o Month, o Year, o Note]
-spec Phdthesis = allOf [e Author, e Title, e School, e Year, o Type, o Address, o Month, o Note]
-spec Proceedings = allOf [e Title, e Year, o Editor, sVN, o Series, o Address, o Month, o Organization, o Publisher, o Note]
-spec Techreport = allOf [e Author, e Title, e Institution, e Year, o Type, o Address, o Month, o Note]
-spec Unpublished = allOf [e Author, e Title, e Note, o Month, o Year]
+spec Article       = allOf [sAE, e Title, e Journal, e Year, o Volume, o Number,
+                            o Pages, o Month, o Note]
+spec Book          = allOf [sAE, e Title, e Publisher, sVN, o Series,
+                            o Address, o Edition, o Month, o Note, e Year]
+spec Booklet       = allOf [e Title, o Author, o HowPublished, o Address, o Month,
+                            o Year, o Note]
+spec Conference    = spec Inproceedings
+spec Inbook        = allOf [sAE, e Title, someOf (e Chapter) (e Pages), e Publisher,
+                            e Year, o Volume, sVN, o Series, o Type, o Address,
+                            o Edition, o Month, o Note]
+spec Incollection  = allOf [e Author, e Title, e Booktitle, e Year, o Editor,
+                            sVN, o Series, o Pages, o Address, o Month,
+                            o Organization, o Publisher, o Note]
+spec Inproceedings = allOf [e Author, e Title, o Editor, e Booktitle, sVN,
+                            o Series, o Pages, o Address, o Month, o Organization,
+                            o Publisher, o Note, e Year]
+spec Manual        = allOf [e Title, o Author, o Organization, o Address,
+                            o Edition, o Month, o Year, o Note]
+spec Mastersthesis = allOf [e Author, e Title, e School, e Year, o Type, o Address,
+                            o Month, o Note]
+spec Misc          = allOf [o Author, o Title, o HowPublished, o Month, o Year, o Note]
+spec Phdthesis     = allOf [e Author, e Title, e School, e Year, o Type,
+                            o Address, o Month, o Note]
+spec Proceedings   = allOf [e Title, e Year, o Editor, sVN, o Series, o Address,
+                            o Month, o Organization, o Publisher, o Note]
+spec Techreport    = allOf [e Author, e Title, e Institution, e Year, o Type,
+                            o Address, o Month, o Note]
+spec Unpublished   = allOf [e Author, e Title, e Note, o Month, o Year]
