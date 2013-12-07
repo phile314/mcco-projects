26f6574dfe4108e3d883fb1d49125bf4532f9482
Marco Vassena <m.vassena@students.uu.nl>
2013-12-07 16:42:16 +0100

    Reogranized following the example of given modules
    Attribute grammars are hidden internally and there is an unique entry point
    to the outer example (Type.hs for type-checking, Diag.hs, Picture.hs)

---
+++ project2/src/TcTDiag.hs
@@ -0,0 +1,9 @@
+-- | The program tc-tdiag
+
+module Main (main) where
+
+import Type
+
+-- | The entry point of the program tc-tdiag
+main :: IO ()
+main = undefined
---
+++ project2/src/Type.hs
@@ -0,0 +1,5 @@
+-- The module that is exported and accessible to the user.
+
+module Type where
+
+import Type.AG
