fc07840228b979d8b82bcc66441d4ed8c659c811
Marco Vassena <m.vassena@students.uu.nl>
2013-12-07 17:49:44 +0100

    Patched CCO.Diag
      Since we want to reuse the Diag datatypes defined in
      src/CCO/Diag/AG/Base.ag we need to include it in
      Type/AG.ag. However we need also to import CCO.Diag
      for importing the Diag type and constructors.
      Since include inlines the content of the file we end up
      with a duplicate instance Tree for Diag and Diag_.
      In order to avoid this such instance has been moved from
      src/CCO/Diag/AG/Base.ag to src/CCO/Diag/Base.hs.
    
      Temporarily the original Makefile has been readded:
      Using only cabal seems not to work correctly.

--- project2/src/Type/Internal.hs
+++ project2/src/Type/Internal.hs
@@ -8,12 +8,12 @@ data Type
   -- | The type of platform t-diagram. 
   | PlatformT { language :: String } -- ^ the implementation language
   -- | The type of an interpreter t-diagram.
-  | Interpreter {target :: String, -- ^ the implementation language 
+  | InterpreterT {target :: String, -- ^ the implementation language 
                  language :: String} -- ^ the language being interpreted.
   -- | The typeof a compiler t-diagram.
-  | Compiler { from :: String, -- ^ the source language,
+  | CompilerT { from :: String, -- ^ the source language,
                to :: String, -- ^ the target language,
                language :: String} -- ^ the implementation language of the compiler.
   -- | A dummy type used for t-diagram that are completely executed.
-  | Unit
+  | UnitT
 
