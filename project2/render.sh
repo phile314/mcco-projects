59b461e039555741eca1dcb14fc1d741a0fa3f8d
Philipp Hausmann <philipp.hausmann@314.ch>
2013-12-12 16:05:52 +0100

    -Picture conversion for execute

--- project2/render.sh
+++ project2/render.sh
@@ -14,13 +14,15 @@ cat input_2.ast | ../dist/build/tdiag2picture/tdiag2picture > input_3.pict
 
 
 cat << "EOF" > input.tex
-\documentclass[12pt, a4paper]{article}
+\documentclass[10pt, a4paper]{article}
 \begin{document}
+\begin{footnotesize}
 EOF
 
 cat input_3.pict | ../dist/build/pp-picture/pp-picture >> input.tex
 
 cat << "EOF" >> input.tex
+\end{footnotesize}
 \end{document}
 EOF
 
