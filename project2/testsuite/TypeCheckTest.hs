df03442a08345ff82f0d25b9999a874590236244
Marco Vassena <m.vassena@students.uu.nl>
2013-12-14 23:22:01 +0100

    Simplified type-check in order to provide precise error messages.

--- project2/testsuite/TypeCheckTest.hs
+++ project2/testsuite/TypeCheckTest.hs
@@ -11,7 +11,7 @@ import Utility (arbitrary)
 
 -- | The tests that will be run
 tests :: [Property]
-tests = [property correctType, property wrongExecute, property wrongCompile]
+tests = [property correctType]
 
 -- | Returns the type of a 'Diag'.
 typeOf :: Diag -> Type
@@ -20,31 +20,14 @@ typeOf d = ty_Syn_Diag $ wrap_Diag (sem_Diag d) (Inh_Diag {})
 -- | Tests that the correct type is returned.
 -- It does *not* test that the t-diagram is well-typed.
 correctType :: Diag -> Bool
-correctType d@(Diag p td) = (expected td) == (typeOf d)
+correctType d@(Diag p td) = if (not $ (expected td) == (typeOf d)) then (error . unwords) (map show [(expected td), (typeOf d)]) else True
   where expected (Platform m) = PlatformT m 
         expected (Program _ l) = ProgramT l UnitT 
         expected (Interpreter _ l m) = ProgramT m (PlatformT l)
         expected (Compiler _ l1 l2 m) = ProgramT m (CompilerT l1 l2)
-        expected (Execute d1 d2) = result $ typeOf d1
+        expected (Execute d1 (Diag _ (Platform _))) = result $ typeOf d1
+        expected (Execute d1 d2) = ProgramT ((language . result . typeOf) d2) ((result . typeOf) d1)
         expected (Compile d1 d2) = ProgramT ((to . result . typeOf) d2) ((result . typeOf) d1)
-
--- | Tests that ill-typed expression combined with compile return a 'TypeError',
-wrongCompile :: Type -> Type -> Bool
-wrongCompile (ProgramT l r) (ProgramT m (CompilerT l1 l2)) | l == l1 = True
-wrongCompile t1@(ProgramT _ _) t2@ErrorT = compile t1 t2 == Right ErrorT
-wrongCompile t1@(ProgramT l r) t2 = compile t1 t2 == (Left $ TypeError t2 [ProgramT "<m>" (CompilerT l "<l2>")])
-wrongCompile t1@ErrorT t2 = compile t1 t2 == Right ErrorT
-wrongCompile t1 t2 = compile t1 t2 == (Left $ TypeError t1 [ProgramT "<l>" UnitT])
-
--- | Tests that ill-typed expression combined with execute return a 'TypeError',
-wrongExecute :: Type -> Type -> Bool
-wrongExecute (ProgramT l r) (PlatformT l') | l == l' = True
-wrongExecute (ProgramT l r) (ProgramT m (PlatformT l')) | l == l' = True
-wrongExecute t1@(ProgramT _ _) t2@ErrorT = execute t1 t2 == Right ErrorT
-wrongExecute t1@(ProgramT l r) t2 = 
-  execute t1 t2 == (Left $ TypeError t2 [PlatformT l])
-wrongExecute t1@ErrorT t2 = execute t1 t2 == Right ErrorT
-wrongExecute t1 t2 = execute t1 t2 == (Left $ TypeError t1 [ProgramT "<l>" UnitT])
 
 -- | The entry point of the testsuite
 main :: IO ()
@@ -53,11 +36,3 @@ main = do
   if all isSuccess results
     then return()
     else exitFailure
-
--------------------------------------------------------------------------------
--- Utility functions
--------------------------------------------------------------------------------
-
--- | Checks whether a type is an error type, e.g. 'ErrorT'.
-isErrorT ErrorT = True
-isErrorT _ = False
