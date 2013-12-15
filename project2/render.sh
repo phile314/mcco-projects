e7aee24acf838caffc3a301936547fb21db33a1f
Philipp Hausmann <philipp.hausmann@314.ch>
2013-12-15 13:28:52 +0100

    -Abort render if a step fails -Workaround, render.sh should work too if called from different directory

--- project2/render.sh
+++ project2/render.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-mkdir -p tmp
+# does not work if there are symlinks etc.
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BIN="${DIR}/dist/build"
 
-rm tmp/input.tdiag
+set -o pipefail
+
+mkdir -p "${DIR}/tmp"
+
+rm "${DIR}/tmp/input.tdiag"
 
 
-cat $1 >> tmp/input.tdiag
+cat $1 >> "${DIR}/tmp/input.tdiag"
 
+OLD_PWD=`pwd`
+cd "${DIR}/tmp"
+cat input.tdiag | "${BIN}/parse-tdiag/parse-tdiag" > input_2.ast
+if [ $? -ne 0 ]; then
+    echo "Parse error, aborting..."
+    exit 2
+fi
 
-cd tmp
-cat input.tdiag | ../dist/build/parse-tdiag/parse-tdiag > input_2.ast
-cat input_2.ast | ../dist/build/tc-tdiag/tc-tdiag
-cat input_2.ast | ../dist/build/tdiag2picture/tdiag2picture > input_3.pict
+cat input_2.ast | "${BIN}/tc-tdiag/tc-tdiag"
+if [ $? -ne 0 ]; then
+    echo "Type checking failed, aborting..."
+    exit 2
+fi
+
+# rendering should never fail, if the above two steps worked
+cat input_2.ast | "${BIN}/tdiag2picture/tdiag2picture" > input_3.pict
 
 
 cat << "EOF" > input.tex
@@ -20,7 +38,7 @@ cat << "EOF" > input.tex
 \begin{footnotesize}
 EOF
 
-cat input_3.pict | ../dist/build/pp-picture/pp-picture >> input.tex
+cat input_3.pict | "${BIN}/pp-picture/pp-picture" >> input.tex
 
 cat << "EOF" >> input.tex
 \end{footnotesize}
@@ -29,4 +47,4 @@ EOF
 
 pdflatex input.tex
 
-cd ..
+cd "${OLD_PWD}"
